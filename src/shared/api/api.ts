7fefa63483daf521e77d400cce87cb3842fe6fe9
Andrew Leykin <a.leykin.kazanexpress@gmail.com>
2024-07-03 14:35:23 +0300

    feat: update api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,9 @@
+import { ManualPaymentController } from './generated-api/ManualPaymentController';
 import { SimcardController } from './generated-api/SimcardController';
 import { TinkoffPaymentController } from './generated-api/TinkoffPaymentController';
 
 export const api = {
   simcard: new SimcardController(),
   tinkoffPayment: new TinkoffPaymentController(),
+  manualPayment: new ManualPaymentController(),
 };
