78ec39e1cebf428349a379778853de069741dcb8
Andrew Leykin <a.leykin.kazanexpress@gmail.com>
2024-07-08 10:15:58 +0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd());
-  
   return {
     resolve: {
       alias: {
@@ -14,7 +12,7 @@ export default defineConfig(({mode}) => {
       },
     },
     plugins: [svgLoader({svgo: false}),vue()],
-    base: env.NODE_ENV === 'production' ? '/ckp/' : '/'
+    base: mode === 'production' ? '/ckp/' : '/'
   }
 
 })
