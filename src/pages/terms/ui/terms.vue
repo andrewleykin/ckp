9cf5c7c42a2942c32dfe88720838861bcbd30d66
Andrew Leykin <a.leykin.kazanexpress@gmail.com>
2024-08-16 11:00:38 +0300

    fix: add terms

--- src/pages/terms/ui/terms.vue
+++ src/pages/terms/ui/terms.vue
@@ -1,9 +1,27 @@
 <template>
-  <main>
+  <main class="terms">
     <ui-typography variant="HeadlineH3">Договор оферты</ui-typography>
+    <div class="image">
+      <img src="./оферта.jpg" />
+    </div>
   </main>
 </template>
 
 <script lang="ts" setup>
 import { UiTypography } from '@/shared/ui/ui-typography';
 </script>
+
+<style lang="scss" scoped>
+.terms {
+  overflow-y: auto;
+}
+
+.image {
+  margin-top: 28px;
+  max-width: 760px;
+
+  img {
+    width: 100%;
+  }
+}
+</style>
